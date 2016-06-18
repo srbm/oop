162ee60911351b5c1e1ff0ab17147100301b6fc0
srbm <srbmwins@gmail.com>
2016-06-17 21:37:05 -0400

    understanding classes

--- inc/classes/recipe.php
+++ inc/classes/recipe.php
@@ -2,9 +2,28 @@
 
 class Recipe 
 {
+	public $title;
+	public $ingredients = array();
+	public $instructions = array();
+	public $yield;
+	public $tag = array();
+	public $source = "Alena Holligan";
+	
+	public function displayRecipe()
+	{
+		return $this->title . " by " . $this->source;
+	}
 	
 }
 
 $recipe1 = new Recipe();
+$recipe1->source = "Grandma Holligan";
+$recipe1->title = "My First Recipe";
 
-var_dump($recipe1);
+
+$recipe2 = new Recipe();
+$recipe2->source = "Betty Crocker";
+$recipe2->title = "My Second Recipe";
+
+echo $recipe1->displayRecipe();
+echo $recipe2->displayRecipe();
