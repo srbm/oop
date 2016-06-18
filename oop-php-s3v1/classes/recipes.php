dce7ab13cb8536d5082aec4dc9b0f34e8956a56d
srbm <srbmwins@gmail.com>
2016-06-18 17:27:27 -0400

    associative arrays

---
+++ oop-php-s3v1/classes/recipes.php
@@ -0,0 +1,59 @@
+<?php
+
+class Recipe
+{
+    private $title;
+    public $ingredients = array();
+    public $instructions = array();
+    public $yield;
+    public $tag = array();
+    public $source = "Alena Holligan";
+	
+		private $measurements = array(
+			"tsp",
+			"tbsp",
+			"cup",
+			"oz",
+			"lb",
+			"fl oz",
+			"pint",
+			"quart",
+			"gallon"
+		);
+	
+	public function setTitle($title)
+	{
+		$this->title = ucwords($title);
+	}
+	
+	public function getTitle()
+	{
+		return $this->title;
+	}
+	
+	public function addIngredient($item, $amont=null, $measure=null)
+	{
+		if ($amount != null && !is_float($amount) && !is_int($amount)) {
+			exit("The amount must be a float: " . gettype($amount) . " $amount given");
+		}
+		if ($measure != null && !in_array(strtolower($measure), $this->measuremnts)) {
+			exit("Please enter a valid measuremnet: " . implode(", ", $this->measurements));
+		}
+		
+		$this->ingredients[] = array(
+			"item" => ucwords($item),
+			"amount" => $amount,
+			"measure" => strtolower($measure),
+		);
+	}
+	
+	public function getIngredients()
+	{
+		return $this->ingredients;
+	}
+    
+    public function displayRecipe()
+    {
+        return $this->title . " by " . $this->source;
+    }
+}
---
+++ oop-php-s3v1/cookbook.php
@@ -0,0 +1,18 @@
+<?php 
+include "classes/recipes.php";
+
+$recipe1 = new Recipe();
+$recipe1->source = "Grandma Holligan";
+$recipe1->setTitle("my first recipe");
+$recipe1->addIngredient("egg", 1);
+$recipe1->addIngredient("flour", 2, cup);
+
+
+$recipe2 = new Recipe();
+$recipe2->source = "Betty Crocker";
+$recipe2->setTitle("My Second Recipe");
+
+echo $recipe1->getTitle();
+foreach ($recipe1->getIngredients() as $ing) {
+	echo "\n" . $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
+}
