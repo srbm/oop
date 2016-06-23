db833f9c4733d23a4512711f8c0267456e431573
srbm <srbmwins@gmail.com>
2016-06-23 18:23:16 -0400

    creating a colleciton

---
+++ oop-php-s4v1/cookbook.php
@@ -0,0 +1,28 @@
+<?php
+include "classes/recipes.php";
+include "classes/render.php";
+include "inc/recipes.php";
+include "classes/recipecollection.php";
+
+	
+$cookbok = new RecipeCollection("Treehouse Recipes");
+$cookbook->addRecipe($lemon_chicken);
+$cookbook->addRecipe($granola_muffins);
+$cookbook->addRecipe($belgian_waffles);
+$pepper_casserole);
+$cookbook->addRecipe($lasagna);
+$cookbook->addRecipe($dried_mushroom_ragout);
+$cookbook->addRecipe($rabbit_catalan);
+$cookbook->addRecipe($grilled_salmon_with_fennel);
+$cookbook->addRecipe($pistachio_duck);
+$cookbook->addRecipe($chili_pork);
+$cookbook->addRecipe($crab_cakes);
+$cookbook->addRecipe($beef_medallions);
+$cookbook->addRecipe($silver_dollar_cakes);
+$cookbook->addRecipe($french_toast);
+$cookbook->addRecipe($corn_beef_hash);
+$cookbook->addRecipe($granola);
+$cookbook->addRecipe($spicy_omelette);
+$cookbook->addRecipe($scones);
+var_dump($cookbook);
+//echo Render::displayRecipe($belgian_waffles);
